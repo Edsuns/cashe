7a14354ab7c7ad4ba12c52c91800e7b0cb72a88e
Edsuns <edsuns@qq.com>
2024-12-16 01:20:20 +0800

    test: some tests

--- src/test/java/io/github/edsuns/CacheManagerTest.java
+++ src/test/java/io/github/edsuns/CacheManagerTest.java
@@ -1,14 +1,7 @@
 package io.github.edsuns;
 
-import io.github.edsuns.redis.ArrayResultTypeScriptExecutor;
-import io.github.edsuns.redis.RedisCacheStorage;
 import io.github.edsuns.util.Some;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.*;
@@ -112,69 +105,45 @@ class CacheManagerTest {
         }
     }
 
-    private final RedisConnectionFactory connectionFactory = getConnectionFactory();
-    private final CacheStorage storage = new RedisCacheStorage(connectionFactory);
-    private final BookDatabase database = new BookDatabase();
-    private final CacheManager<Book, Long> bookCacheManager = new CacheManagerImpl<>(storage, database);
+    private TestContext testContext = new TestContext();
 
     @Test
     void test() {
         // prepare
         String keyValue = "cashe:book:2";
         String keyVersion = "cashe:book:2:v";
-        RedisTemplate<String, Object> redis = createRedisTemplate(connectionFactory);
-        bookCacheManager.delete(Collections.singletonList(2L));
+        testContext.getBookCacheManager().delete(Collections.singletonList(2L));
 
-        List<Book> books = bookCacheManager.getByIds(Collections.singletonList(2L));
+        List<Book> books = testContext.getBookCacheManager().getByIds(Collections.singletonList(2L));
         assertEquals(1, books.size());
         assertEquals(new Book(2L, "book2"), books.get(0));
 
-        database.update(new HashMap<>() {{
+        testContext.getDatabase().update(new HashMap<>() {{
             put(2L, new Book(2L, "book2_2"));
         }});
-        bookCacheManager.scheduledInvalidateUpdated();
-        List<Book> books1 = bookCacheManager.getByIds(Collections.singletonList(2L));
+        testContext.getBookCacheManager().scheduledInvalidateUpdated();
+        List<Book> books1 = testContext.getBookCacheManager().getByIds(Collections.singletonList(2L));
         assertEquals(1, books1.size());
         assertEquals(new Book(2L, "book2_2"), books1.get(0));
 
-        bookCacheManager.setNulls(Collections.singletonList(2L));
-        List<Book> books2 = bookCacheManager.getByIds(Collections.singletonList(2L));
+        testContext.getBookCacheManager().setNulls(Collections.singletonList(2L));
+        List<Book> books2 = testContext.getBookCacheManager().getByIds(Collections.singletonList(2L));
         assertEquals(1, books2.size());
         assertNull(books2.get(0));
 
-        assertEquals(new Some(null), redis.opsForValue().get(keyValue));
-        assertEquals(3, redis.opsForValue().get(keyVersion));
+        assertEquals(new Some(null), testContext.getRedis().opsForValue().get(keyValue));
+        assertEquals(3, testContext.getRedis().opsForValue().get(keyVersion));
 
-        bookCacheManager.updateByIds(new HashMap<>() {{
+        testContext.getBookCacheManager().updateByIds(new HashMap<>() {{
             put(2L, new Book(2L, "book2_3"));
         }});
-        assertEquals(new Some(new Book(2L, "book2_3")), redis.opsForValue().get(keyValue));
-        storage.invalidate(Collections.singletonList(keyValue));
+        assertEquals(new Some(new Book(2L, "book2_3")), testContext.getRedis().opsForValue().get(keyValue));
+        testContext.getStorage().invalidate(Collections.singletonList(keyValue));
 
-        assertNull(redis.opsForValue().get(keyValue));
-        assertEquals(5, redis.opsForValue().get(keyVersion));
+        assertNull(testContext.getRedis().opsForValue().get(keyValue));
+        assertEquals(5, testContext.getRedis().opsForValue().get(keyVersion));
 
         // rollback
-        bookCacheManager.delete(Collections.singletonList(2L));
-    }
-
-    private static LettuceConnectionFactory getConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("127.0.0.1");
-        configuration.setPort(6379);
-        configuration.setPassword("eMtyKVge8oLd2t81");
-        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(configuration);
-        connectionFactory.afterPropertiesSet();
-        return connectionFactory;
-    }
-
-    private static RedisTemplate<String, Object> createRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(RedisSerializer.string());
-        template.setValueSerializer(RedisSerializer.json());
-        template.setScriptExecutor(new ArrayResultTypeScriptExecutor<>(template));
-        template.afterPropertiesSet();
-        return template;
+        testContext.getBookCacheManager().delete(Collections.singletonList(2L));
     }
 }
---
+++ src/test/java/io/github/edsuns/TestContext.java
@@ -0,0 +1,58 @@
+package io.github.edsuns;
+
+import io.github.edsuns.redis.ArrayResultTypeScriptExecutor;
+import io.github.edsuns.redis.RedisCacheStorage;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author edsuns@qq.com
+ * @since 2024/1/26 17:49
+ */
+public class TestContext {
+
+    private final RedisConnectionFactory connectionFactory = getConnectionFactory();
+    private final CacheStorage storage = new RedisCacheStorage(connectionFactory);
+    private final CacheManagerTest.BookDatabase database = new CacheManagerTest.BookDatabase();
+    private final CacheManager<CacheManagerTest.Book, Long> bookCacheManager = new CacheManagerImpl<>(storage, database);
+    private final RedisTemplate<String, Object> redis = createRedisTemplate(getConnectionFactory());
+
+    public CacheStorage getStorage() {
+        return storage;
+    }
+
+    public CacheManagerTest.BookDatabase getDatabase() {
+        return database;
+    }
+
+    public CacheManager<CacheManagerTest.Book, Long> getBookCacheManager() {
+        return bookCacheManager;
+    }
+
+    public RedisTemplate<String, Object> getRedis() {
+        return redis;
+    }
+
+    private static LettuceConnectionFactory getConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("127.0.0.1");
+        configuration.setPort(6379);
+        configuration.setPassword("eMtyKVge8oLd2t81");
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(configuration);
+        connectionFactory.afterPropertiesSet();
+        return connectionFactory;
+    }
+
+    private static RedisTemplate<String, Object> createRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
+        template.setScriptExecutor(new ArrayResultTypeScriptExecutor<>(template));
+        template.afterPropertiesSet();
+        return template;
+    }
+}
